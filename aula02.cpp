f831178c36fe944527a42a802cbb0f278b769a96
karinamaria <karinamaria163@gmail.com>
2020-06-17 10:50:04 -0300

    Correção de erros.

--- aula02.cpp
+++ aula02.cpp
@@ -2,20 +2,26 @@
 #include <string>
 
 int main(int argc, char *argv[]) {
-	std::string mensagem;
 	
-	if(argc == 1 || (argc > 1 && std::string(argv[1]) != "add")){
+	if(argc == 1){
 		std::cout << "Uso: " << argv[0] << " add <mensagem>" << std::endl;
-	}else if(argc > 2){
-		for(int i = 2; i < argc; i++){
-			mensagem.append(argv[i]);
-			mensagem.append(" ");
-		}
-		std::cout << mensagem << std::endl;
-	}else{
+		return 1;
+	}
+
+	std::string primeiroArgumento = argv[1];
+	if(primeiroArgumento != "add"){
+		std::cout << "Uso: " << argv[0] << " add <mensagem>" << std::endl;
+		return 1;
+	}
+
+	std::string mensagem;
+	if(argc == 2){
 		std::cout << "Informe a mensagem: ";
 		std::getline(std::cin, mensagem);
-		std::cout << mensagem << std::endl;
+	}else{
+		mensagem = argv[2];
 	}
+	std::cout << "Mensagem adicionada!" << std::endl;
+	
   	return 0;
 }
