826d3086a6062b98a7d5dbe11c292e80081024c6
karinamaria <karinamaria163@gmail.com>
2020-06-18 12:06:37 -0300

    aula04

--- main.cpp
+++ main.cpp
@@ -19,26 +19,16 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	std::ifstream arquivo_entrada("arquivo.txt");
-	std::ofstream arquivo_saida("arquivo.txt", std::ios::app);
 	if(acao == "add"){
 		std::string mensagem = (argc == 2) ? "" : argv[2];
 		if(argc == 2){
 			std::cout << "Informe a mensagem: ";
 			std::getline(std::cin, mensagem);
 		}
-		if (!arquivo_saida.is_open()) {
-	   		std::cerr << "Arquivo não existente ou sem permissão de leitura." << std::endl;
-	    	return 1;
-  		}
-		adicionarMensagemNoArquivo(arquivo_saida, mensagem);
+		return adicionarMensagemNoArquivo(mensagem);
 	}
 	if(acao == "list"){
-		if (!arquivo_entrada.is_open()) {
-	   		std::cerr << "Arquivo não existente ou sem permissão de leitura." << std::endl;
-	    	return 1;
-  		}
-		lerArquivo(arquivo_entrada);
+		return lerArquivo();
 	}
 	
 	
@@ -50,24 +40,39 @@ void mostrarUso(const std::string &nomePrograma) {
   std::cout << "Uso: " << nomePrograma << " list" << std::endl;
 }
 
-void adicionarMensagemNoArquivo(std::ifstream arquivo_saida, std::string mensagem){
-	
-	arquivo_saida << mensagem << std::endl;
+int adicionarMensagemNoArquivo(std::string mensagem){
+	std::ofstream arquivo("arquivo.txt", std::ios::app);
+
+	if (!arquivo.is_open()) {
+   		std::cerr << "Arquivo não existente ou sem permissão de leitura." << std::endl;
+    	return 1;
+  	}
+	arquivo << mensagem << std::endl;
 	std::cout << "Mensagem adicionada!" << std::endl;
+
+	return 0;
 }
 
-int lerArquivo(std::ifstream arquivo_entrada){
+int lerArquivo(){
+	std::ifstream arquivo("arquivo.txt");
+
+	if (!arquivo.is_open()) {
+   		std::cerr << "Arquivo não existente ou sem permissão de leitura." << std::endl;
+    	return 1;
+  	}
   	std::string message;
 
   	int numeroLinha = 0;
 
-  	while (!arquivo_entrada.eof()) {
+  	while (!arquivo.eof()) {
     	++numeroLinha;
-    	std::getline(arquivo_entrada, message);
+    	std::getline(arquivo, message);
     	if (message.size() == 0) { //Contar quantidade de bytes da linha
       		continue;//se a qnt de bytes for zero, é iniciada a próxima interação do while
     	}
 
     	std::cout << numeroLinha << ". " << message << std::endl;
   	}
+
+  	return 0;
 }
