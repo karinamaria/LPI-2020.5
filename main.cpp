ebee4016d441c5e7b40443d5a608b95da10cd3fb
karinamaria <karinamaria163@gmail.com>
2020-06-19 10:05:14 -0300

    correções.

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 #include <string>
 
 void mostrarUso(const std::string &nomePrograma);
-int adicionarMensagemNoArquivo(std::string mensagem);
-int lerArquivo();
+void adicionarMensagemNoArquivo(std::ofstream &arquivo, std::string mensagem);
+void lerArquivo(std::ifstream &arquivo);
 
 int main(int argc, char *argv[]) {
 
@@ -19,16 +19,27 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
+	std::ifstream arquivo_entrada("arquivo.txt");
+	std::ofstream arquivo_saida("arquivo.txt", std::ios::app);
+
 	if(acao == "add"){
 		std::string mensagem = (argc == 2) ? "" : argv[2];
 		if(argc == 2){
 			std::cout << "Informe a mensagem: ";
 			std::getline(std::cin, mensagem);
 		}
-		return adicionarMensagemNoArquivo(mensagem);
+		if (!arquivo_saida.is_open()) {
+   			std::cerr << "Arquivo não existente ou sem permissão de leitura." << std::endl;
+    		return 1;
+  		}
+		adicionarMensagemNoArquivo(arquivo_saida, mensagem);
 	}
-	if(acao == "list"){
-		return lerArquivo();
+	else if(acao == "list"){
+		if (!arquivo_entrada.is_open()) {
+   			std::cerr << "Arquivo não existente ou sem permissão de leitura." << std::endl;
+    		return 1;
+  		}
+		lerArquivo(arquivo_entrada);
 	}
 	
 	
@@ -40,26 +51,14 @@ void mostrarUso(const std::string &nomePrograma) {
   std::cout << "Uso: " << nomePrograma << " list" << std::endl;
 }
 
-int adicionarMensagemNoArquivo(std::string mensagem){
-	std::ofstream arquivo("arquivo.txt", std::ios::app);
-
-	if (!arquivo.is_open()) {
-   		std::cerr << "Arquivo não existente ou sem permissão de leitura." << std::endl;
-    	return 1;
-  	}
+void adicionarMensagemNoArquivo(std::ofstream &arquivo, std::string mensagem){
+	
 	arquivo << mensagem << std::endl;
 	std::cout << "Mensagem adicionada!" << std::endl;
 
-	return 0;
 }
 
-int lerArquivo(){
-	std::ifstream arquivo("arquivo.txt");
-
-	if (!arquivo.is_open()) {
-   		std::cerr << "Arquivo não existente ou sem permissão de leitura." << std::endl;
-    	return 1;
-  	}
+void lerArquivo(std::ifstream &arquivo){
   	std::string message;
 
   	int numeroLinha = 0;
@@ -74,5 +73,4 @@ int lerArquivo(){
     	std::cout << numeroLinha << ". " << message << std::endl;
   	}
 
-  	return 0;
 }
